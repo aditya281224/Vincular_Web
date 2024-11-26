6906b747a547ec42d1406a16844934077fb8b90e
Aditya Sharma <adityasharmavats2811@gmail.com>
2024-11-26 10:59:03 +0530

    deploy error fixes

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ cloudinary.config({
 });
 
 const app = express()
-const PORT = process.env.PORT || PORT
-const__dirname=path.resolve()
+const PORT = process.env.PORT || 5000
+const __dirname=path.resolve()
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended:true}))
